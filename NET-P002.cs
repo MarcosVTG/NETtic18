6b0c7ecd9daaf3963a9d3825aa28e38f6117de00
Marcos <mvtgomes.ege@uesc.br>
2023-11-19 12:51:51 -0300

    implementado classe gerenciador de tarefas

--- NET-P002.cs
+++ NET-P002.cs
@@ -10,3 +10,26 @@ class Tarefa
     public DateTime DataVencimento { get; set; }
     public bool Concluida { get; set; }
 }
+
+class GerenciadorTarefas
+{
+    private static List<Tarefa> tarefas = new List<Tarefa>();
+
+    static void Main(string[] args)
+    {
+        bool emExecucao = true;
+        while (emExecucao)
+        {
+            ExibirMenu();
+            if (int.TryParse(Console.ReadLine(), out int opcao))
+            {
+                ProcessarOpcao(opcao);
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida! Por favor, digite um número válido.");
+            }
+        }
+    }
+
+}
