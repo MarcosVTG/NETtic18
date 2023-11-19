e992955027f652a9abe436bf6b1175e779251a53
Marcos <mvtgomes.ege@uesc.br>
2023-11-19 12:50:41 -0300

    Implementado classe tarefa

---
+++ NET-P002.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+class Tarefa
+{
+    public string Titulo { get; set; }
+    public string Descricao { get; set; }
+    public DateTime DataVencimento { get; set; }
+    public bool Concluida { get; set; }
+}
