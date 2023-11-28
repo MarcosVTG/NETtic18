0395029cba80a178e117bf552d0a264f785c1651
Marcos <mvtgomes.ege@uesc.br>
2023-11-28 20:26:25 -0300

    Implementado demais metódos e funções

--- NET-P003.cs
+++ NET-P003.cs
@@ -29,3 +29,55 @@ static List<(string Codigo, string Nome, int Quantidade, double Preco)> Iniciali
             ("P003", "Mouse", 30, 25.0)
         };
     }
+    static void ImprimirEstoque(List<(string Codigo, string Nome, int Quantidade, double Preco)> estoque)
+    {
+        foreach (var produto in estoque)
+        {
+            Console.WriteLine($"Código: {produto.Codigo}, Nome: {produto.Nome}, Quantidade: {produto.Quantidade}, Preço: {produto.Preco}");
+        }
+    }
+
+    static void AdicionarProduto(List<(string Codigo, string Nome, int Quantidade, double Preco)> estoque, (string, string, int, double) dadosProduto)
+    {
+        estoque.Add(dadosProduto);
+        Console.WriteLine($"\nProduto '{dadosProduto.Nome}' adicionado ao estoque.");
+    }
+
+    static void AtualizarQuantidade(List<(string Codigo, string Nome, int Quantidade, double Preco)> estoque, string codigoProduto, int novaQuantidade)
+    {
+        var produto = estoque.FindIndex(p => p.Codigo == codigoProduto);
+        if (produto != -1)
+        {
+            var produtoAtualizado = estoque[produto];
+            produtoAtualizado.Quantidade = novaQuantidade;
+            estoque[produto] = produtoAtualizado;
+            Console.WriteLine($"\nQuantidade do produto '{produtoAtualizado.Nome}' atualizada para {novaQuantidade}.");
+        }
+        else
+        {
+            Console.WriteLine($"\nProduto com código '{codigoProduto}' não encontrado no estoque.");
+        }
+    }
+
+    static void RemoverProduto(List<(string Codigo, string Nome, int Quantidade, double Preco)> estoque, string codigoProduto)
+    {
+        var produto = estoque.FindIndex(p => p.Codigo == codigoProduto);
+        if (produto != -1)
+        {
+            var produtoRemovido = estoque[produto];
+            estoque.RemoveAt(produto);
+            Console.WriteLine($"\nProduto '{produtoRemovido.Nome}' removido do estoque.");
+        }
+        else
+        {
+            Console.WriteLine($"\nProduto com código '{codigoProduto}' não encontrado no estoque.");
+        }
+    }
+
+    static void ConsultarProdutosComPrecoSuperiorA(List<(string Codigo, string Nome, int Quantidade, double Preco)> estoque, double valorLimite)
+    {
+        var produtosFiltrados = estoque.Where(p => p.Preco > valorLimite);
+        Console.WriteLine($"\nProdutos com preço superior a {valorLimite}:");
+        ImprimirEstoque(produtosFiltrados.ToList());
+    }
+}
